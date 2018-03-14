b45613810b93967e60945295187c55eaed11045d
xiaobolo <xiaobolo@163.com>
2018-03-14 10:14:40 +0800

    实现事件简单的演示

---
+++ EventDemo/EventDemo/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EventDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Person person;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            person = new Person();
+            person.ChangeNameEvent += Person_ChangeNameEvent;
+        }
+
+        private void Person_ChangeNameEvent()
+        {
+            button1.Content = person.Name;
+            Console.WriteLine("事件触发");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            person.Name = "Xiaobolo";
+        }
+    }
+}
---
+++ EventDemo/EventDemo/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDemo
+{
+    class Person
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnNameChanged();
+            }
+        }
+
+        public delegate void DelegateHandler();
+
+        public event DelegateHandler ChangeNameEvent;
+
+        public void OnNameChanged()
+        {
+            ChangeNameEvent?.Invoke();
+        }
+    }
+}
